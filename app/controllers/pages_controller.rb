bff97c12316386d2709871760e6d0017e96f682d
Nimit Jain <zennimit@gmail.com>
2015-05-16 10:04:14 +0530

    Created home & about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def gogo
+  end
+  def haha
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "yo" => "pages#yoyo"
+  get "haha" => "pages#gogo"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
